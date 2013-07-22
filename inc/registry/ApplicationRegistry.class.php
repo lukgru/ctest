36a377c37e812425e399364c43a1673371054bc7
pearnet <pearnetpl@gmail.com>
2013-07-22 20:41:00 +0200

    fix application registry class

--- inc/registry/ApplicationRegistry.class.php
+++ inc/registry/ApplicationRegistry.class.php
@@ -1,43 +1,49 @@
 <?php
+
 /*
  * Handle base application settings
  */
+
 namespace inc\registry;
 
 class ApplicationRegistry extends \inc\registry\Registry
 {
+
     private static $instance;
     private $properties = array();
 
     protected function __construct()
     {
-        $this->set("ROOT_DIR", dirname(dirname(dirname(__FILE__)))."/");
-        $this->set("CONTROLLERS_DIR", dirname(dirname(dirname(__FILE__)))."/inc/controllers/");
+
     }
-    
+
     public function set($key, $value)
     {
-	$this->properties[$key] = $value;
+        $this->properties[$key] = $value;
     }
 
     public function get($key)
     {
-	return isset($this->properties[$key]) ? $this->properties[$key] : null;
+        return isset($this->properties[$key]) ? $this->properties[$key] : null;
     }
 
     public function check($key)
     {
-	return ($this->get($key) !== null);
+        return ($this->get($key) !== null);
     }
-    
+
     public function getInstance()
     {
         if(!self::$instance)
         {
             self::$instance = new self();
+            self::$instance->set("ROOT_DIR", dirname(dirname(dirname(__FILE__)))."/");
+            self::$instance->set("CONTROLLERS_DIR", dirname(dirname(dirname(__FILE__)))."/inc/controllers/");
         }
 
         return self::$instance;
     }
+
 }
+
 ?>
